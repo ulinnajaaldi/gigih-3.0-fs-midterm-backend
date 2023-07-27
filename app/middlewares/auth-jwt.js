2086e38ffc0d7e8ed06870791268ea4ff52318b3
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-07-28 02:49:21 +0700

    feat - creating jwt middleware

---
+++ app/middlewares/auth-jwt.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+exports.authToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
